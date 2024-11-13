14f86fe5dfc38f6386662f134fd1a1d28319f699
shquers <shquers@gmail.com>
2024-11-13 19:09:33 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoListComponent } from './todo-management/todo-list/todo-list.component';
+import { InsertComponent } from './todo-management/insert/insert.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TodoManagementComponent } from './todo-management/todo-management.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'todo-management', component: TodoManagementComponent },
+];
